58fe379abbbf44d2c77cd4c8e2c5e029953bb056
EG <103245945+EmanuelGauci@users.noreply.github.com>
2026-06-22 07:34:47 +0200

    Update telescope.lua
    Moved telescope keybinds into dedicated plugin config

--- nvim/lua/conf/lazy-plugins/telescope.lua
+++ nvim/lua/conf/lazy-plugins/telescope.lua
@@ -9,11 +9,24 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
-        require('telescope').load_extension('project')
-
         local builtin = require('telescope.builtin')
+        require('telescope').setup({})
+
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+        vim.keymap.set('n', '<leader>fs', function()
+            builtin.grep_string({search = vim.fn.input("Grep > ")});
+        end)
+
+        require('telescope').load_extension('project')
+        vim.keymap.set('n', '<leader>ps', ":lua require 'telescope' .extensions.project.project{}<CR>", {noremap = true, silent = true})
+
+
+
+
         vim.deprecate = function() end
     end
 }
-
